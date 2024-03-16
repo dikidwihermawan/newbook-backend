75c614269741c896b1ebfdf8aac75ef8b9448e17
Diki Dwi Hermawan <dikidwihermawan@gmail.com>
2024-03-16 12:55:47 +0700

    fix api

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    public function index()
+    public function login()
     {
         return response()->json('hello world');
     }
--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('login', [LoginController::class])->name('login');
+Route::post('login', [LoginController::class, 'login'])->name('login');
