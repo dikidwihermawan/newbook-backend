faad7d63b9bc20b5ec68ce826397ed1cb27a4b16
Diki Dwi Hermawan <dikidwihermawan@gmail.com>
2024-03-26 16:07:55 +0700

    add redux

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,14 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\MeController;
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
     Route::post('register', [RegisterController::class, 'register'])->name('register');
     Route::post('login', [LoginController::class, 'login'])->name('login');
+});
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/me', MeController::class);
 });
